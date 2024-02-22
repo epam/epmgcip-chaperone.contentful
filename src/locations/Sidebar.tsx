9e8a7e5408c557eca7754f3dbce583cb67e6bb69
Siarhei Balonikau <siarhei_balonikau@epam.com>
2024-02-22 12:54:24 +0400

    'EPMGCIP-17/feature/add-qrcode-with-image'

--- src/locations/Sidebar.tsx
+++ src/locations/Sidebar.tsx
@@ -1,56 +1,115 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { SidebarAppSDK } from '@contentful/app-sdk';
-import QRCode from 'qrcode';
-import { saveAs } from 'file-saver';
-import { useSDK } from '@contentful/react-apps-toolkit';
-import styles from './Sidebar.module.css';
-import { Button, Note } from '@contentful/f36-components';
+import QRCodeStyling from "qr-code-styling";
+import { useSDK } from "@contentful/react-apps-toolkit";
+import styles from "./Sidebar.module.css";
+import { Button, Note } from "@contentful/f36-components";
+import logo from "../assets/logo.png";
 
-const CONTENT_FIELD_ID = 'slug';
+const CONTENT_FIELD_ID = "slug";
+const QR_CODE_WIDTH = 2000;
+const QR_CODE_HEIGHT = 2000;
+
+const qrCode = new QRCodeStyling({
+  width: QR_CODE_WIDTH,
+  height: QR_CODE_HEIGHT,
+});
+
+const qrCodeWithImage = new QRCodeStyling({
+  width: QR_CODE_WIDTH,
+  height: QR_CODE_HEIGHT,
+  image: logo,
+  imageOptions: {
+    margin: 8,
+  },
+});
 
 const Sidebar = () => {
   const sdk = useSDK<SidebarAppSDK>();
-  const componentRef = useRef(null);
+  const qrCodeRef = useRef<any>(null);
+  const qrCodeWithImageRef = useRef<any>(null);
   const entry = sdk.entry;
   const contentField = entry.fields[CONTENT_FIELD_ID];
-  const [qrCode, setQrCode] = useState<string | null>(null);
-  const [isPublished, setIsPublished] = useState<boolean>(entry.getSys().publishedBy !== undefined);
+  const [isPublished, setIsPublished] = useState<boolean>(
+    entry.getSys().publishedBy !== undefined
+  );
+
+  useEffect(() => {
+    qrCode.append(qrCodeRef.current);
+    qrCodeWithImage.append(qrCodeWithImageRef.current);
+  }, []);
 
   useEffect(() => {
     const detach = contentField.onValueChanged(async (slug) => {
-      const url = `${process.env.REACT_APP_DOMAIN}/${slug}`
-      const qrCode = await QRCode.toDataURL(url);
-      setQrCode(qrCode);
+      const url = `${process.env.REACT_APP_DOMAIN}/${slug}`;
+
+      qrCode.update({
+        data: url,
+      });
+
+      qrCodeWithImage.update({
+        data: url,
+      });
+
+      sdk.window.updateHeight();
     });
     return () => detach();
-  }, [contentField]);
+  }, [contentField, sdk.window]);
 
   useEffect(() => {
     const detach = entry.onSysChanged(async (sys) => {
-      setIsPublished(sys.publishedBy !== undefined)
+      setIsPublished(sys.publishedBy !== undefined);
     });
     return () => detach();
   }, [entry]);
 
-  const handleClick = () => {
-    if (qrCode) {
-      saveAs(qrCode, contentField.getValue());
-    }
-  }
+  const handleSaveQrCodeClick = () => {
+    qrCode.download({
+      name: contentField.getValue(),
+      extension: "png",
+    });
+  };
+
+  const handleSaveQrCodeWithImageClick = () => {
+    qrCodeWithImage.download({
+      name: contentField.getValue(),
+      extension: "png",
+    });
+  };
 
   return (
     <>
-      {qrCode && isPublished && <>
-        <img className={styles.qrcode} ref={componentRef} src={qrCode} onLoad={() => sdk.window.updateHeight()} alt="qr code"/>
-        Test
-        <Button variant="primary" size="medium" isFullWidth onClick={handleClick}>Save QR Code</Button>
-      </>}
+      {qrCode && isPublished && (
+        <>
+          <div className={styles.qrcode} ref={qrCodeRef} />
+          <Button
+            variant="primary"
+            size="medium"
+            isFullWidth
+            onClick={handleSaveQrCodeClick}
+          >
+            Save QR Code
+          </Button>
+        </>
+      )}
 
-      {!isPublished &&
-        <Note variant="primary">
-          Please, "publish" the article first.
-        </Note>
-      }
+      {qrCodeWithImage && isPublished && (
+        <div className={styles.qrCodeWithImageSectionWrapper}>
+          <div className={styles.qrcode} ref={qrCodeWithImageRef} />
+          <Button
+            variant="primary"
+            size="medium"
+            isFullWidth
+            onClick={handleSaveQrCodeWithImageClick}
+          >
+            Save QR Code
+          </Button>
+        </div>
+      )}
+
+      {!isPublished && (
+        <Note variant="primary">Please, "publish" the article first.</Note>
+      )}
     </>
   );
 };
