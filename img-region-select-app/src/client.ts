9e2c73c41ff564fb569d65e400a5087c3ec3357c
Siarhei Balonikau <sergeibolonikov@gmail.com>
2024-09-11 18:46:06 +0400

    fix/epmgcip-143/updating-env-variables (#18)
    Co-authored-by: Siarhei Balonikau <siarhei_balonikau@epam.com>

--- img-region-select-app/src/client.ts
+++ img-region-select-app/src/client.ts
@@ -2,7 +2,8 @@ import { createClient } from "contentful";
 
 const client = createClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE || "",
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN || "",
+  accessToken:
+    process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN || "",
 });
 
 export default client;
